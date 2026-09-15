8ddb959feb9156e176585b2f9f62300d579f7257
ankurchauhan889911-tech <ankurchauhan889911@gmail.com>
2026-09-15 20:00:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ app.use(express.static('public'));
 const AD_SCRIPT = `
 <script>
   atOptions = {
-    'key' : '72b6f3ac3fc2f43722e5f2196ef85add',
+    'key' : '1e88cc42ffb63328764e50a5268f9415',
     'format' : 'iframe',
     'height' : 90,
     'width' : 728,
     'params' : {}
   };
 </script>
-<script src="https://www.highperformanceformat.com/72b6f3ac3fc2f43722e5f2196ef85add/invoke.js"></script>
+<script src="https://www.highperformanceformat.com/1e88cc42ffb63328764e50a5268f9415/invoke.js"></script>
 `;
 
 const AD_TOP = `
@@ -152,14 +152,14 @@ ${AD_TOP}
   <div>
     <script>
       atOptions = {
-        'key' : 'd1b072857c7132ec474a48b3413701e2',
+        'key' : 'c12ae2c2919125ebdc7f1ee9002c1456',
         'format' : 'iframe',
         'height' : 60,
         'width' : 468,
         'params' : {}
       };
     </script>
-    <script src="https://www.highperformanceformat.com/d1b072857c7132ec474a48b3413701e2/invoke.js"></script>
+    <script src="https://www.highperformanceformat.com/c12ae2c2919125ebdc7f1ee9002c1456/invoke.js"></script>
   </div>
 </div>
 ${bodyContent}
